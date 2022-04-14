a323062b3b176a59349ec152f6df20b4356379fb
Aidan Yee <aidan.yee@pm.me>
2022-04-14 16:04:37 -0700

    added logic to handle if there is an error & breed not found

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,10 +4,22 @@ const request = require('request');
 const breedName = process.argv[2]; // cli aregument
 
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function (error, response, body) {
-  console.log("error:", error);
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, response, body) {
+
+  if (error) {
+    console.log("Error. There is an issue with the API. Please try again.");
+    return;
+  }
+
   const data = JSON.parse(body);
-  //console.log("data.weight: ", data[0].weight); //data is an array not an object! need to acess the array, then the object
-  console.log("Breed Description:", data[0].description)
+  // console.log('data:', data.length)
+  // console.log("typeOfbody: ",typeof body)
+
+  if (data.length === 0) {
+    console.log("Breed not found");
+    return;
+  }
+  //data is an array not an object! need to acess the array, then the object
+  console.log("Breed Description:", data[0].description);
 });
 
